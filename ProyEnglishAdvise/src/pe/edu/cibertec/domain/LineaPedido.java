64a2f767413e34775024336e84782e8b590fc397
FERNANDO <fnomad7@gmail.com>
2017-06-12 23:46:44 -0500

    Clases LineaPedido y Pedido creadas para el proceso de carro de compra.

---
+++ ProyEnglishAdvise/src/pe/edu/cibertec/domain/LineaPedido.java
@@ -0,0 +1,63 @@
+package pe.edu.cibertec.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_lineaPedido")
+public class LineaPedido {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	@ManyToOne
+	private Pedido pedido;
+	@ManyToOne
+	private Producto producto;
+	private Integer cantidad;
+	private double valorTotal;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public double getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+}
---
+++ ProyEnglishAdvise/src/pe/edu/cibertec/domain/Pedido.java
@@ -0,0 +1,43 @@
+package pe.edu.cibertec.domain;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_pedido")
+public class Pedido {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private Date fecha;
+	private Cliente cliente;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+}
